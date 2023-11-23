3c335478c6fe6ce579783e9fe23a86e3bc342532
PabloNovaes <ppablo.0028922@gmail.com>
2023-11-23 18:56:36 -0300

    update deployment config

--- src/server.js
+++ src/server.js
@@ -28,7 +28,11 @@ const app = express();
 app.use(timeout(20000));
 
 //webhook-route
-app.post("/checkout-succeded", checkoutController.updateOrderStatus);
+app.post(
+  "/checkout-succeded",
+  express.raw({ type: "application/json" }),
+  checkoutController.updateOrderStatus
+);
 
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ limit: "20mb", extended: true }));
