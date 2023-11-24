e054d66696670bbb32f25771344fbf778e9ba17b
Pablo Novaes <133129982+PabloNovaes@users.noreply.github.com>
2023-11-24 02:21:36 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -27,11 +27,10 @@ const app = express();
 // app.use(allowOnlyHost);
 app.use(timeout(20000));
 
-app.use(express.urlencoded({ limit: "20mb", extended: true }));
-
 //webhook-route
-app.post("/checkout-succeded", checkoutController.updateOrderStatus);
+app.post("/checkout-succeded", express.raw({type: "application/json"}),checkoutController.updateOrderStatus);
 
+app.use(express.urlencoded({ limit: "20mb", extended: true }));
 app.use(express.json({ limit: "20mb" }));
 app.use(cookieParser());
 app.use(routes);
