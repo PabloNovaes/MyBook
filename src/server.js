a4ef9612a35faa5a8f6aaacac495100e1ca7e9c4
PabloNovaes <ppablo.0028922@gmail.com>
2023-11-24 02:38:15 -0300

    fix: stripe webhook error

--- src/server.js
+++ src/server.js
@@ -28,10 +28,10 @@ const app = express();
 app.use(timeout(20000));
 
 //webhook-route
-app.post("/checkout-succeded", express.raw({type: "application/json"}),checkoutController.updateOrderStatus);
 
 app.use(
   express.json({
+    limit: "20mb",
     verify: (req, res, buf) => {
       req.rawBody = buf;
     },
@@ -42,7 +42,6 @@ app.use(
 app.post("/checkout-succeded", checkoutController.updateOrderStatus);
 
 app.use(express.urlencoded({ limit: "20mb", extended: true }));
-app.use(express.json({ limit: "20mb" }));
 app.use(cookieParser());
 app.use(routes);
 app.use(cors());
