63cb538c9a11717e1a5de101bf8e5595def5aeb1
PabloNovaes <ppablo.0028922@gmail.com>
2023-11-24 01:53:22 -0300

    fix: stripe webhook error

--- src/server.js
+++ src/server.js
@@ -26,7 +26,6 @@ const app = express();
 
 // app.use(allowOnlyHost);
 app.use(timeout(20000));
-app.use(express.json({ limit: "20mb" }));
 
 //webhook-route
 app.post(
@@ -36,6 +35,7 @@ app.post(
 );
 
 app.use(express.urlencoded({ limit: "20mb", extended: true }));
+app.use(express.json({ limit: "20mb" }));
 app.use(cookieParser());
 app.use(routes);
 app.use(cors());
