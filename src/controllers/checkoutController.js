3a9a38b8748355a144e4973487720531df72fa21
PabloNovaes <ppablo.0028922@gmail.com>
2023-11-24 02:33:33 -0300

    fix: stripe webhook error

--- src/controllers/checkoutController.js
+++ src/controllers/checkoutController.js
@@ -84,7 +84,7 @@ export class CheckoutController {
     }
 
     const hooKey = process.env.STRIPE_WEBHOOK_KEY;
-    const event = stripe.webhooks.constructEvent(req.rawbody, signature, hooKey);
+    const event = stripe.webhooks.constructEvent(req.rawBody, signature, hooKey);
 
     if (event.type === "checkout.session.completed") {
       const session = event.data.object;
