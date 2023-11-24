93dd2f7d52cb8a3d720bbe74f83db208093ab547
PabloNovaes <ppablo.0028922@gmail.com>
2023-11-24 03:33:52 -0300

    fix: stripe webhook error

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 import cookieParser from "cookie-parser";
 import routes from "./routes/index.js";
-import timeout from "connect-timeout";
 import express from "express";
 import cors from "cors";
 import path from "path";
@@ -15,29 +14,24 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app = express();
 
-// Configuração de timeout para todas as solicitações
-app.use(timeout(20000));
+// Remova o middleware global de timeout
 
 // Middleware para verificar se é a rota "/checkout-succeded" e aplicar json parsing apenas para essa rota
-app.use((req, res, next) => {
-  if (req.originalUrl === "/checkout-succeded") {
-    return checkoutController.updateOrderStatus(req, res, next);
+app.use(
+  "/checkout-succeded",
+  express.json({ limit: "20mb" }),
+  (req, res, next) => {
+    checkoutController.updateOrderStatus(req, res, next);
   }
-  express.json({ limit: "20mb" })(req, res, next);
-});
+);
 
 // Rota do webhook
-app.post("/checkout-succeded", (req, res) => {
-  // Aqui você pode adicionar lógica adicional, se necessário
-  res.sendStatus(200);
-});
+app.post("/checkout-succeded", checkoutController.updateOrderStatus);
 
 // Restante do middleware e configurações
 app.use(express.urlencoded({ limit: "20mb", extended: true }));
 app.use(cookieParser());
-app.use(routes);
 app.use(cors());
-
 app.use(express.static(path.join(__dirname, "app")));
 app.set("views", path.join(__dirname, "app"));
 app.set("view engine", "ejs");
