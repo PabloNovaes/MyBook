d263799351d5253233d8dd13bbb4e60a23363cca
PabloNovaes <ppablo.0028922@gmail.com>
2023-11-29 18:19:09 -0300

    update deployment links

--- src/app/services/axios/host.js
+++ src/app/services/axios/host.js
@@ -1,5 +1,5 @@
 const NGROK_HOST = "";
-const BASE_HOST = "https://my-book-delta.vercel.app";
+const BASE_HOST = "https://my-book-git-main-pablo-novaes-projects.vercel.app";
 const HOST = NGROK_HOST !== "" ? NGROK_HOST : BASE_HOST;
 
 export default HOST;
