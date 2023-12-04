93f73c31d704ac3014a372bf028a7f33a40eaa0a
PabloNovaes <ppablo.0028922@gmail.com>
2023-12-04 20:48:51 -0300

    update deployment variables

--- src/app/services/axios/host.js
+++ src/app/services/axios/host.js
@@ -1,5 +1,5 @@
 const NGROK_HOST = "";
-const BASE_HOST = "https://f920-2804-388-c359-9bc6-e0f8-d0ae-f7e5-7ae6.ngrok-free.app";
+const BASE_HOST = "https://mybook-iota.vercel.app";
 const HOST = NGROK_HOST !== "" ? NGROK_HOST : BASE_HOST;
 
 export default HOST;
