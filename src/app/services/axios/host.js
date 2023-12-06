2db7de2238a91ca400e187b5bd851f1dd704988c
PabloNovaes <ppablo.0028922@gmail.com>
2023-12-06 16:22:36 -0300

    update host url

--- src/app/services/axios/host.js
+++ src/app/services/axios/host.js
@@ -1,5 +1,5 @@
 const NGROK_HOST = "";
-const BASE_HOST = "http://192.168.0.174:5500";
+const BASE_HOST = "https://my-book-ten.vercel.app/";
 const HOST = NGROK_HOST !== "" ? NGROK_HOST : BASE_HOST;
 
 export default HOST;
