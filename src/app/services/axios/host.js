f9c03305e3d74835217f1f481a95b156ab3a206a
PabloNovaes <ppablo.0028922@gmail.com>
2023-12-06 17:11:50 -0300

    fix: header css

--- src/app/services/axios/host.js
+++ src/app/services/axios/host.js
@@ -1,5 +1,5 @@
 const NGROK_HOST = "";
-const BASE_HOST = "https://my-book-blond.vercel.app";
+const BASE_HOST = "https://mybook-rho.vercel.app";
 const HOST = NGROK_HOST !== "" ? NGROK_HOST : BASE_HOST;
 
 export default HOST;
